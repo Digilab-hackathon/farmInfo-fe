d269a655342b5bbe61d952e52b9e1778a209e030
silver0108 <kimsoeun0108@gmail.com>
2024-11-29 13:21:02 +0900

    feat: 옵션 데이터 분리

---
+++ src/constants/options.ts
@@ -0,0 +1,28 @@
+export const cropOptions = [
+  { label: '품목을 선택해 주세요.', value: '' },
+  { label: '월동무', value: 'RADISH' },
+  { label: '양배추', value: 'CABBAGE' },
+  { label: '당근', value: 'CARROT' },
+  { label: '마늘', value: 'GARLIC' },
+  { label: '양파', value: 'ONION' },
+  { label: '브로콜리', value: 'BROCCOLI' },
+  { label: '비트', value: 'BEET' },
+  { label: '쪽파', value: 'GREEN_ONION' },
+  { label: '콜라비', value: 'KOHLRABI' },
+  { label: '월동배추', value: 'WINTER_CABBAGE' },
+  { label: '적채', value: 'RED_CABBAGE' },
+  { label: '방울다다기양배추', value: 'BRUSSELS_SPROUTS' }
+]
+
+export const landCategoryOptions = [
+  { label: "'지목'을 작성해 주세요.", value: '' },
+  { label: '논', value: 'PADDY_FIELD' },
+  { label: '밭', value: 'FIELD' },
+  { label: '과수원', value: 'ORCHARD' },
+  { label: '목장', value: 'MEADOW' }
+]
+
+export const selfCultivatedOptions = [
+  { label: '자경', value: 'SELF_OWNED' },
+  { label: '매수', value: 'PURCHASED' }
+]
