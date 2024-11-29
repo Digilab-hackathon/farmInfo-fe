2d827640461bd9fe83b415bec1a44171b73be21e
silver0108 <kimsoeun0108@gmail.com>
2024-11-29 10:17:23 +0900

    feat: 재배면적 신고 정보 전역 상태 관리

---
+++ src/store/useCultivatedAreaStore.ts
@@ -0,0 +1,51 @@
+import {
+  CultivatedAreaReportInfo,
+  CultivatedAreaUserInfo
+} from '@/types/CultivatedAreaInfoValues'
+import { create } from 'zustand'
+
+type CultivatedArea = CultivatedAreaUserInfo & CultivatedAreaReportInfo
+
+interface CultivatedAreaState {
+  currentPage: number
+  cultivatedAreaData: CultivatedArea
+  nextPage: () => void
+  updateUserInfo: (member: CultivatedAreaUserInfo) => void
+  updateReportInfo: (report: CultivatedAreaReportInfo) => void
+}
+
+export const useCultivatedAreaStore = create<CultivatedAreaState>(set => ({
+  currentPage: 1,
+  cultivatedAreaData: {
+    reporterName: '',
+    birthday: '',
+    address: '',
+    phone: '',
+    secondPhone: '',
+    productName: '',
+    region: '',
+    ri: '',
+    landCategory: '',
+    area: 0,
+    detailedArea: 0,
+    isSelfCultivated: ''
+  },
+  nextPage: () =>
+    set(state => ({
+      currentPage: state.currentPage + 1
+    })),
+  updateUserInfo: memberInfo =>
+    set(state => ({
+      cultivatedAreaData: {
+        ...state.cultivatedAreaData,
+        ...memberInfo
+      }
+    })),
+  updateReportInfo: reportInfo =>
+    set(state => ({
+      cultivatedAreaData: {
+        ...state.cultivatedAreaData,
+        ...reportInfo
+      }
+    }))
+}))
