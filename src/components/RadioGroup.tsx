c70cbf7662fa436c17ff141e23f760be69215ec3
silver0108 <kimsoeun0108@gmail.com>
2024-11-30 10:10:07 +0900

    style: RadioGroup 스타일링

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useController, Control, Path, FieldValues } from 'react-hook-form'
+import style from '@/styles/components/RadioGroup.module.scss'
+import Image from 'next/image'
 
 interface RadioGroupProps<T extends FieldValues> {
   name: Path<T>
@@ -17,35 +19,43 @@ const RadioGroup = <T extends FieldValues>({
   options,
   rules
 }: RadioGroupProps<T>) => {
-  const {
-    field,
-    fieldState: { error }
-  } = useController<T>({
+  const { field } = useController<T>({
     name,
     control,
     rules
   })
 
   return (
-    <div>
+    <div className={style.radioGroupWrapper}>
       <label>
         <span>{label} </span>
         {rules && '*'}
       </label>
-      <div>
+      <div className={style.radioGroup}>
         {options.map(option => (
-          <label key={option.value}>
+          <label
+            key={option.value}
+            className={style.optionLabel}>
+            <Image
+              src={`${
+                field.value === option.value
+                  ? '/icons/radio-checked.png'
+                  : '/icons/radio-default.png'
+              }`}
+              alt="기본 라디오 버튼"
+              width={20}
+              height={20}
+            />
             <input
               type="radio"
               value={option.value}
               checked={field.value === option.value}
               onChange={field.onChange}
             />
-            {option.label}
+            <span>{option.label}</span>
           </label>
         ))}
       </div>
-      {error && <span>{error.message}</span>}
     </div>
   )
 }
