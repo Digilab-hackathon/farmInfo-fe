7190426e71ab81d4764deee32458233a1d8047f1
silver0108 <kimsoeun0108@gmail.com>
2024-11-30 09:34:50 +0900

    refactor: pathname으로 Footer 각 아이콘 접근

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -2,15 +2,13 @@
 
 import Image from 'next/image'
 import style from './style.module.scss'
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 const Footer = () => {
   const router = useRouter()
-  const [selected, setSelected] = useState('/')
+  const pathname = usePathname()
 
   const handleSelect = (page: string) => {
-    setSelected(page)
     router.push(page)
   }
   return (
@@ -18,21 +16,21 @@ const Footer = () => {
       <section onClick={() => handleSelect('/')}>
         <Image
           src={
-            selected === '/' ? '/icons/mydata-active.png' : '/icons/mydata.png'
+            pathname === '/' ? '/icons/mydata-active.png' : '/icons/mydata.png'
           }
           alt="로고"
           width={24}
           height={24}
           priority
         />
-        <div style={{ color: selected === '/' ? 'black' : '#989685' }}>
+        <div style={{ color: pathname === '/' ? 'black' : '#989685' }}>
           마이데이터
         </div>
       </section>
       <section onClick={() => handleSelect('/shipment-report')}>
         <Image
           src={
-            selected === '/shipment-report'
+            pathname.includes('/shipment-report')
               ? '/icons/shipment-report-active.png'
               : '/icons/shipment-report.png'
           }
@@ -41,14 +39,17 @@ const Footer = () => {
           height={24}
           priority
         />
-        <div style={{ color: selected === '/shipment' ? 'black' : '#989685' }}>
+        <div
+          style={{
+            color: pathname.includes('/shipment-report') ? 'black' : '#989685'
+          }}>
           출하량 신고
         </div>
       </section>
       <section onClick={() => handleSelect('/area-report')}>
         <Image
           src={
-            selected === '/area-report'
+            pathname.includes('/area-report')
               ? '/icons/cultivated-report-active.png'
               : '/icons/cultivated-report.png'
           }
@@ -59,7 +60,7 @@ const Footer = () => {
         />
         <div
           style={{
-            color: selected === '/cultivated-report' ? 'black' : '#989685'
+            color: pathname.includes('/area-report') ? 'black' : '#989685'
           }}>
           재배면적 신고
         </div>
